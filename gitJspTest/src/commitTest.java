b7983b17838c7cf6f97c9d7b319f81acb05769d1
simyang <chnhj1013@gmail.com>
2014-05-30 17:51:50 +0900

    aaaaa

--- gitJspTest/src/commitTest.java
+++ gitJspTest/src/commitTest.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class commitTest
  */
-@WebServlet("/commitTest")
+@WebServlet("/gitTest")
 public class commitTest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
